59735715fe88d6cbf99e2079f5ad75347a254786
christiantadam <63576449+christiantadam@users.noreply.github.com>
2023-09-13 14:53:03 +0700

    Update penyesuaian-sp-ekspor.js
    Salah kirim parameter fetch

--- public/js/Sales/penyesuaian-sp-ekspor.js
+++ public/js/Sales/penyesuaian-sp-ekspor.js
@@ -470,7 +470,7 @@ delete_button.addEventListener("click", function (event) {
     event.preventDefault();
     proses = 2; //karena ini penyesuaian
     let selectedRow = $("#list_view tbody tr.selected");
-    console.log(selectedRow.find("td").eq(17).text());
+    console.log(selectedRow.find("td").eq(16).text());
     let table = $("#list_view").DataTable();
     if (proses == 1) {
         if (selectedRow.length > 0) {
@@ -484,7 +484,7 @@ delete_button.addEventListener("click", function (event) {
         }
     } else if (proses == 2) {
         if (selectedRow.length > 0) {
-            if (selectedRow.find("td").eq(17).text() !== "") {
+            if (selectedRow.find("td").eq(16).text() !== "") {
                 // console.log(input[7].value);
                 let confirmation = confirm(
                     "Anda yakin akan menghapus data ini dari server?"
@@ -493,7 +493,7 @@ delete_button.addEventListener("click", function (event) {
                 if (confirmation) {
                     fetch(
                         "/deleteDetailBarangEksport/" +
-                            selectedRow.find("td").eq(17).text()
+                            selectedRow.find("td").eq(16).text()
                     )
                         .then((response) => response.json())
                         .then((data) => {
