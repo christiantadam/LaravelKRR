dd2d2d09ab94e01c4931e356e424ab6040b65cad
christiantadam <63576449+christiantadam@users.noreply.github.com>
2023-10-06 10:00:09 +0700

    Update batal SP

--- app/Http/Controllers/Sales/Transaksi/SuratPesanan/SuratPesananController.php
+++ app/Http/Controllers/Sales/Transaksi/SuratPesanan/SuratPesananController.php
@@ -106,7 +106,7 @@ class SuratPesananController extends Controller
                 $csrfToken = Session::get('_token');
                 $nestedData['Actions'] = "<button class=\"btn btn-sm btn-info\" onclick=\"openNewWindow('/penyesuaian/" . $no_spValue . "')\">&#x270E; Penyesuaian</button>
                 <br> <form onsubmit=\"return confirm('Apakah Anda Yakin ?');\"
-                                        action=\"http://127.0.0.1:8000/batalsplokal/" . $no_spValue . "\" method=\"POST\"
+                                        action=\"/batalsplokal/" . $no_spValue . "\" method=\"POST\"
                                         enctype=\"multipart/form-data\"> <button type=\"submit\"
                                             class=\"btn btn-sm btn-danger\"><span>&#x1F5D1;</span>Batal SP</button>
                                             <input type=\"hidden\" name=\"_token\" value=\"" . $csrfToken . "\">
--- resources/views/Sales/home.blade.php
+++ resources/views/Sales/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.appSales')
 @section('content')
-@section('title', 'HomeSales')
+@section('title', 'Home Sales')
 
     <div class="container-fluid">
         <div class="row justify-content-center">
