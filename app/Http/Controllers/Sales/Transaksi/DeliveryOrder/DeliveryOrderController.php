2e83b35fd3acbe5511cd8e3b89669ddaa14f7526
christiantadam <63576449+christiantadam@users.noreply.github.com>
2024-01-20 12:15:51 +0700

    Update DeliveryOrderController.php
    Update permohonan DO supaya bisa dipakai export

--- app/Http/Controllers/Sales/Transaksi/DeliveryOrder/DeliveryOrderController.php
+++ app/Http/Controllers/Sales/Transaksi/DeliveryOrder/DeliveryOrderController.php
@@ -49,7 +49,7 @@ class DeliveryOrderController extends Controller
 
     public function getBarang($idPesanan)
     {
-        $data = db::connection('ConnSales')->select('exec SP_1486_SLS_LIST_SALDO_TYPE_DO1 @IDPesanan = ?', [$idPesanan]);
+        $data = db::connection('ConnSales')->select('exec SP_1486_SLS_LIST_SALDO_TYPE_DO1 @IDPesanan = ?, @Kode = ?', [$idPesanan, 1]);
         return response()->json($data);
     }
     public function getKelompokUtama($kodeBarang)
