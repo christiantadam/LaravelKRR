36c49d63529b2c276dd9a7966ecc8b31872efe13
christiantadam <63576449+christiantadam@users.noreply.github.com>
2025-03-08 10:37:03 +0700

    fix <Multiple Program>
    Tombol timbang disabled untuk user aulia dan ika

--- public/js/MultipleProgram/KonversiRollBarcodeABMStghJadi.js
+++ public/js/MultipleProgram/KonversiRollBarcodeABMStghJadi.js
@@ -529,7 +529,8 @@ $(document).ready(function () {
         if (
             nomorUser !== "4384" && //adam
             nomorUser !== "4199" && //kelvin
-            nomorUser !== "1516" //sri gayati
+            nomorUser !== "2244" && //ika
+            nomorUser !== "4428" //aulia
         ) {
             hasil_konversiTritierTujuan.readOnly = true;
             button_timbangTujuanKonversi.disabled = false;
