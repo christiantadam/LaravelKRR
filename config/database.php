1594407f48e04134692a081ce45ad253c0c0993f
christiantadam <63576449+christiantadam@users.noreply.github.com>
2025-07-05 10:53:58 +0700

    fix<Beli>
    Create BTTB Insert into database PURCHASETRIAL

--- config/database.php
+++ config/database.php
@@ -258,6 +258,21 @@ return [
             'encrypt' => 'yes',
             'trust_server_certificate' => true,
         ],
+
+        'ConnPurchaseTrial' => [
+            'driver' => 'sqlsrv',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST_THIRTEEN', '127.0.0.1'),
+            'port' => env('DB_PORT_THIRTEEN', '1433'),
+            'database' => env('DB_DATABASE_THIRTEEN', 'forge'),
+            'username' => env('DB_USERNAME_THIRTEEN', 'forge'),
+            'password' => env('DB_PASSWORD_THIRTEEN', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'encrypt' => 'yes',
+            'trust_server_certificate' => true,
+        ],
     ],
 
     /*
