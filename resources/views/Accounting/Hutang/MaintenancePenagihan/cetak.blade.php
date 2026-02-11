4cc43263d6205aee389531574f59cc2d1483785c
christiantadam <63576449+christiantadam@users.noreply.github.com>
2026-02-11 09:11:16 +0700

    fix<Accounting>
    penyesuaian hasil cetak request dari user endah

--- resources/views/Accounting/Hutang/MaintenancePenagihan/cetak.blade.php
+++ resources/views/Accounting/Hutang/MaintenancePenagihan/cetak.blade.php
@@ -17,8 +17,8 @@
                 font-size: 11px;
             }
 
-            table tr td {
-                border: 1px solid black;
+            #table_dataSPPB tbody tr:nth-last-child(2) {
+                border-bottom: 1px solid black;
             }
         </style>
     </head>
@@ -26,12 +26,13 @@
     <body>
         <div>
             <div style="display: flex;flex-direction: row;">
-                <div style="display: flex;flex-direction: column;flex: 0.5;text-align: left;">
+                <div style="display: flex;flex-direction: column;flex: 0.5;text-align: left;font-size: 13px;">
                     <label>PT. KERTA RAJASA RAYA</label>
                     <label>Jl. Raya Tropodo No. 1</label>
                     <label>Waru - Sidoarjo</label>
+                    {{-- <label>Phone: +62-31-8669595, Fax: +62-31-8675315</label> --}}
                 </div>
-                <table style="flex: 0.5;border-collapse: collapse;font-size: 11px;">
+                <table style="flex: 0.5;border-collapse: collapse;font-size: 12px;">
                     <tr>
                         <td style="border:none;" colspan="3">Receipt Invoice</td>
                     </tr>
@@ -88,18 +89,38 @@
                     <label>PHONE: {{ $dataCetak[0]->TLP1 }}, FAX: {{ $dataCetak[0]->FAX1 ?? '-' }}</label>
                 </div>
             </div>
-            <table style="width: 100%;border-collapse: collapse;margin-top: 10px;font-size: 11px">
+            <table id="table_dataSPPB" style="width: 100%;border-collapse: collapse;margin-top: 10px;font-size: 11px">
                 <tr style="white-space: nowrap">
-                    <td style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;">No.</td>
-                    <td style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;">Receive Date</td>
-                    <td style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;">Receipt No.</td>
-                    <td style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;">PO</td>
-                    <td style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;">Description</td>
-                    <td style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;">Qty</td>
-                    <td style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;">Unit</td>
-                    <td style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;">Unit Price</td>
-                    <td style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;">Discount</td>
-                    <td style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;">Amount
+                    <td
+                        style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;border-bottom: 1px solid black;">
+                        No.</td>
+                    <td
+                        style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;border-bottom: 1px solid black;">
+                        Receive Date</td>
+                    <td
+                        style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;border-bottom: 1px solid black;">
+                        Receipt No.</td>
+                    <td
+                        style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;border-bottom: 1px solid black;">
+                        PO</td>
+                    <td
+                        style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;border-bottom: 1px solid black;">
+                        Description</td>
+                    <td
+                        style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;border-bottom: 1px solid black;">
+                        Qty</td>
+                    <td
+                        style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;border-bottom: 1px solid black;">
+                        Unit</td>
+                    <td
+                        style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;border-bottom: 1px solid black;">
+                        Unit Price</td>
+                    <td
+                        style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;border-bottom: 1px solid black;">
+                        Discount</td>
+                    <td
+                        style="padding: 10px 5px 10px 5px; text-align: center;font-weight: bold;border-bottom: 1px solid black;">
+                        Amount
                         {{ $dataCetak[0]->Id_MataUang_BC }}
                     </td>
                 </tr>
@@ -118,22 +139,22 @@
                                     : $item->Harga_Terbayar;
                             $subTotal += (float) $hargaMurni - $hargaDisc;
                         @endphp
-                        <td style="padding: 0 5px 0 5px">{{ $index + 1 }}</td>
-                        <td style="padding: 0 5px 0 5px; white-space: nowrap;">
+                        <td style="padding: 2px 5px 0 5px">{{ $index + 1 }}</td>
+                        <td style="padding: 2px 5px 0 5px; white-space: nowrap;">
                             {{ date('d-M-Y', strtotime($item->Datang)) }}</td>
-                        <td style="padding: 0 5px 0 5px; white-space: nowrap;">{{ $item->No_SuratJalan }}</td>
-                        <td style="padding: 0 5px 0 5px; white-space: nowrap;">{{ $item->No_PO }}</td>
-                        <td style="padding: 0 5px 0 5px">{{ $item->NAMA_BRG }}</td>
-                        <td style="padding: 0 5px 0 5px">{{ number_format($item->Qty_Terima, 0, '.', ',') }}</td>
-                        <td style="padding: 0 5px 0 5px">{{ $item->SatTerima }}</td>
-                        <td style="padding: 0 5px 0 5px; white-space: nowrap;">
+                        <td style="padding: 2px 5px 0 5px;">{{ $item->No_SuratJalan }}</td>
+                        <td style="padding: 2px 5px 0 5px; white-space: nowrap;">{{ $item->No_PO }}</td>
+                        <td style="padding: 2px 5px 0 5px">{{ $item->NAMA_BRG }}</td>
+                        <td style="padding: 2px 5px 0 5px">{{ number_format($item->Qty_Terima, 0, '.', ',') }}</td>
+                        <td style="padding: 2px 5px 0 5px">{{ $item->SatTerima }}</td>
+                        <td style="padding: 2px 5px 0 5px; white-space: nowrap;">
                             {{-- {{ $item->Symbol }} --}}
                             {{ number_format($hargaSatuan, 4, '.', ',') }}
                         </td>
-                        <td style="padding: 0 5px 0 5px; white-space: nowrap;">
+                        <td style="padding: 2px 5px 0 5px; white-space: nowrap;">
                             {{-- {{ $item->Symbol }} --}}
                             {{ number_format($hargaDisc, 2, '.', ',') }}</td>
-                        <td style="padding: 0 5px 0 5px; white-space: nowrap;">
+                        <td style="padding: 2px 5px 0 5px; white-space: nowrap;">
                             {{-- {{ $item->Symbol }} --}}
                             {{ number_format($hargaMurni, 4, '.', ',') }}
                         </td>
@@ -148,20 +169,20 @@
                         $ppnAmount = ($dppAmount * $Nilai_Pajak) / 100;
                     @endphp
                     <tr>
-                        <td colspan="9" style="border: none;text-align: right;padding-right: 5px;">Subtotal</td>
-                        <td style="border: none;padding: 0 0 0 5px;">{{ $dataCetak[0]->Symbol }}
+                        <td colspan="9" style="border: none;text-align: right;padding: 5px 5px 0 0;">Subtotal</td>
+                        <td style="border: none;padding: 5px 0 0 5px;">{{ $dataCetak[0]->Symbol }}
                             {{ number_format($subTotal, 2, '.', ',') }} </td>
                     </tr>
                     @if ($subTotal !== $dppAmount)
                         <tr>
-                            <td colspan="9" style="border: none;text-align: right;padding-right: 5px;">DPP</td>
-                            <td style="border: none;padding: 0 0 0 5px;">{{ $dataCetak[0]->Symbol }}
+                            <td colspan="9" style="border: none;text-align: right;padding: 5px 5px 0 0;">DPP</td>
+                            <td style="border: none;padding: 5px 0 0 5px;">{{ $dataCetak[0]->Symbol }}
                                 {{ number_format($dppAmount, 2, '.', ',') }} </td>
                         </tr>
                     @endif
                     <tr>
-                        <td colspan="9" style="border: none;text-align: right;padding-right: 5px;">PPN</td>
-                        <td style="border: none;padding: 0 0 0 5px;">{{ $dataCetak[0]->Symbol }}
+                        <td colspan="9" style="border: none;text-align: right;padding: 5px 5px 0 0;">PPN</td>
+                        <td style="border: none;padding: 5px 0 0 5px;">{{ $dataCetak[0]->Symbol }}
                             {{ number_format($ppnAmount, 2, '.', ',') }} </td>
                     </tr>
                     @php
@@ -169,8 +190,8 @@
                         $total = (float) $subTotal + (float) $ppnAmount;
                     @endphp
                     <tr>
-                        <td colspan="9" style="border: none;text-align: right;padding-right: 5px;">Total</td>
-                        <td style="border: none;padding: 0 0 0 5px;white-space: nowrap;">{{ $dataCetak[0]->Symbol }}
+                        <td colspan="9" style="border: none;text-align: right;padding: 5px 5px 0 0;">Total</td>
+                        <td style="border: none;padding: 5px 0 0 5px;white-space: nowrap;">{{ $dataCetak[0]->Symbol }}
                             {{ number_format($total, 4, '.', ',') }} </td>
                     </tr>
                 @else
@@ -178,21 +199,21 @@
                         $total = $subTotal;
                     @endphp
                     <tr>
-                        <td colspan="9" style="border: none;text-align: right;padding-right: 5px;">Total</td>
-                        <td style="border: none;padding: 0 0 0 5px;white-space: nowrap;">{{ $dataCetak[0]->Symbol }}
+                        <td colspan="9" style="border: none;text-align: right;padding: 5px 5px 0 0;">Total</td>
+                        <td style="border: none;padding: 5px 0 0 5px;white-space: nowrap;">{{ $dataCetak[0]->Symbol }}
                             {{ number_format($total, 4, '.', ',') }} </td>
                     </tr>
                     @if ($dataCetak[0]->Id_MataUang_BC !== 'IDR')
                         <tr>
-                            <td colspan="9" style="border: none;text-align: right;padding-right: 5px;">
+                            <td colspan="9" style="border: none;text-align: right;padding: 5px 5px 0 0;;">
                                 Kurs IDR</td>
-                            <td style="border: none;padding: 0 0 0 5px;">Rp.
+                            <td style="border: none;padding: 5px 0 0 5px;">Rp.
                                 {{ number_format($dataCetak[0]->Kurs_Rp, 2, '.', ',') }} </td>
                         </tr>
                         <tr>
-                            <td colspan="9" style="border: none;text-align: right;padding-right: 5px;">
+                            <td colspan="9" style="border: none;text-align: right;padding: 5px 5px 0 0;;">
                                 Total IDR</td>
-                            <td style="border: none;padding: 0 0 0 5px;white-space: nowrap;">Rp.
+                            <td style="border: none;padding: 5px 0 0 5px;white-space: nowrap;">Rp.
                                 {{ number_format($dataCetak[0]->Harga_TerbayarRp, 2, '.', ',') }} </td>
                         </tr>
                     @endif
