35ae77ff21735b6d7b889f34baea877b9ea71cca
christiantadam <63576449+christiantadam@users.noreply.github.com>
2026-03-06 09:03:17 +0700

    fix<Home>
    bootstrap load tidak pakai cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -247,8 +247,6 @@
         @auth
         @include('modalTambahPengumuman')
         @endauth
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
     </body>
 
 </html>
